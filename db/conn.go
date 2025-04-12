2e5dce30bed59a306f4f3f3508674477636c296b
aryapuja <aryapuja2103@gmail.com>
2025-04-12 08:27:53 +0700

    deploy-fix: rename var @conn

--- db/conn.go
+++ db/conn.go
@@ -18,11 +18,11 @@ func ConnectDB() {
 	// dsn := "host=localhost port=5432 user=postgres password=qwer1234 dbname=bioskop sslmode=disable"
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
 	)
 	DB, err = sql.Open("postgres", dsn)
 	if err != nil {
